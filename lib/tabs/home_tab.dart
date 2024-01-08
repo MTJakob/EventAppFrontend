167b9b7203fdc618b716b648d684688ebb1d3646
Aleksander <olek2534@gmail.com>
2024-01-08 18:27:20 +0100

    Map added

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class HomeTab extends StatefulWidget {
   const HomeTab({super.key});
@@ -10,6 +12,32 @@ class HomeTab extends StatefulWidget {
 class _HomeTabState extends State<HomeTab> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: ListView.builder(itemBuilder: (_, index){})),
+        Expanded(
+          flex: 1,
+          child: Card(
+            shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(40))),
+            clipBehavior: Clip.hardEdge,
+            elevation: 10,
+            child: FlutterMap(options: const MapOptions(
+              initialCenter: LatLng(38.1858, 15.5561),
+              initialZoom: 17
+              ), 
+            children: [
+              TileLayer(
+                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                userAgentPackageName: 'com.example.app',
+              )
+              ]),
+          ),
+        )
+        ],
+      ),
+    );
   }
 }
