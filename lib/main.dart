b99cb64cc27ed9747438dffebd718ca39d949397
Aleksander <olek2534@gmail.com>
2024-01-08 21:10:40 +0100

    Added the main inherited widget

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,32 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       darkTheme: ThemeData.dark(),
-      home: const MainPage(),
+      home: const MyAppData(child: MainPage(),),
     );
+  }
+}
+
+
+class MyAppData extends InheritedWidget {
+  const MyAppData({super.key, required super.child});
+
+  /* 
+  this is the main container for future variables like session keys, cache managers...
+  define a variable like:
+  final double sampledata = 15;
+  reference it in a child widget:
+  MyAppData.of(context).sampledata
+  */
+
+  @override
+  bool updateShouldNotify(InheritedWidget oldWidget) => true;
+
+  static MyAppData? maybeOf(BuildContext context) => 
+    context.dependOnInheritedWidgetOfExactType<MyAppData>();
+
+  static MyAppData of(BuildContext context) {
+    final MyAppData? result = maybeOf(context);
+    assert(result != null, 'No MyAppData found');
+    return result!;
   }
 }
